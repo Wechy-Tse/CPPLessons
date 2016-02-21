119a7adfc624a3442b78f8572936dc589efef4cc
Wechy-Tse <1033881020@qq.com>
2016-02-21 19:55:07 +0800

    CPPLessons-01
    20160221

---
+++ code/ide/xcode/L01OOP/L01OOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L01OOP
+//
+//  Created by 谢伟智 on 16/2/21.
+//  Copyright © 2016年 Wechy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World123!\n";
+    return 0;
+}
---
+++ code/ide/xcode/L02NS/L02NS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L02NS
+//
+//  Created by 谢伟智 on 16/2/21.
+//  Copyright © 2016年 Wechy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
